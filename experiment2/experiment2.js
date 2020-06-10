595f5106fbb5d3cb2e64e10f3aecde7f4cd812e0
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2020-06-10 22:19:39 +0200

    nieuwe versie, schonere code?

--- experiment2/experiment2.js
+++ experiment2/experiment2.js
@@ -1,4 +1,5 @@
 import { ARButton } from 'https://unpkg.com/three/examples/jsm/webxr/ARButton.js';
+import { TTFLoader } from 'https://unpkg.com/three/examples/jsm/loaders/TTFLoader.js';
 
 let arDisplay, renderer, scene, arView, camera, arControls;
 let arObjects = [];
@@ -15,10 +16,10 @@ function init(display) {
     const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
     light.position.set( 0, 1, 0 );
     scene.add( light );
-    scene.add(new THREE.AmbientLight(0xffffff,0.5))
+    scene.add(new THREE.AmbientLight(0xffffff, 0.5))
 
     console.log("add text");
-    const text_loader = new THREE.FontLoader()
+    const text_loader = new TTFLoader()
     text_loader.load("./font/Otsutome_font_ver3/OtsutomeFont_Ver3.ttf", data => {
         const font = new THREE.FontLoader().parse(data)
         const geo = new THREE.TextBufferGeometry("MIW", { //"祝う"
@@ -36,14 +37,26 @@ function init(display) {
         );
         mesh.position.set(0, 1.5, -7);
 
+        console.log("adding font mesh")
         scene.add(mesh);
         arObjects.push(mesh);
     });
 
+    let cube = new THREE.Mesh(
+        new THREE.BoxBufferGeometry(1,1,1),
+        new THREE.MeshLambertMaterial({color:'red'})
+    );
+    cube.position.set(0, 1.5, -10);
+    scene.add(cube);
+    arObjects.push(cube);
+
     console.log("setup renderer");
     // setup ARView with ARPerspectiveCamera
     renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.xr.enabled = true;
+    document.body.appendChild(renderer.domElement);
 
     console.log("setup view");
     // arView = new THREE.ARView(arDisplay, renderer);
@@ -52,6 +65,8 @@ function init(display) {
     console.log("setup camera");
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
     arControls = renderer.xr.getController(0);
+    arControls.userData.skipFrames = 0;
+    scene.add( arControls );
 
     console.log("animate!")
     animate();
@@ -64,7 +79,6 @@ function animate() {
 }
 
 function render(time) {
-    console.log("render")
     // arControls.update();
 
     arObjects.forEach((item, index) => updateHeight(item, time));
@@ -77,9 +91,11 @@ THREE.ARUtils.getARDisplay().then(init);
 
 
 
-function updateHeight(character, time) {
-    character.translateY(-.000001 * time);
-    if (character.getWorldPosition().y < -2) {
-        character.translateY(4);
+function updateHeight(arObject, time) {
+    arObject.translateY(-.000001 * time);
+    let position = arObject.getWorldPosition(new THREE.Vector3());
+
+    if (position.y < -2) {
+        arObject.translateY(4);
     }
 }
